bcb071f47daf10a2cd88523fff63e13d8f1d848d
nehir-mentes <nehirmen.98@gmail.com>
2025-04-10 20:11:00 +0000

    xx

--- app.rb
+++ app.rb
@@ -2,10 +2,7 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:homepage)
 end
 
 get("/rock") do
@@ -20,6 +17,38 @@ get("/rock") do
     @outcome = "won"
   end
 
-  erb{:zebra}
+  erb(:zebra)
+
+end
+
+  get("/paper") do
+    moves = ["rock", "paper", "scissors"]
+    @comp_move = moves.sample
+  
+    if @comp_move == "paper"
+      @outcome = "tied"
+    elsif @comp_move == "scissors"
+      @outcome = "lost"
+    else
+      @outcome = "won"
+    end
+  
+    erb(:giraffe)
+
+end
+
+get("/scissors") do
+  moves = ["rock", "paper", "scissors"]
+  @comp_move = moves.sample
+
+  if @comp_move == "scissors"
+    @outcome = "tied"
+  elsif @comp_move == "rock"
+    @outcome = "lost"
+  else
+    @outcome = "won"
+  end
+
+  erb(:puppy)
 
 end
